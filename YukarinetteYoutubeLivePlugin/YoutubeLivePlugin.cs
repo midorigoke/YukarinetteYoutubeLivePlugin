e0b4b92d1324f2ff45a85e6aa4b69433bd6cf570
midorigoke <midorigoke@desumachi.tk>
2017-02-15 00:53:32 +0900

    initial commit

---
+++ YukarinetteYoutubeLivePlugin/YoutubeLivePlugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yukarinette;
+
+namespace YukarinetteYoutubeLivePlugin
+{
+    public class YoutubeLivePlugin : IYukarinetteInterface
+    {
+		private ConfigManager mConfigManager;
+
+		private YoutubeLiveManager mYoutubeLiveManager;
+
+		public override string Name
+		{
+			get
+			{
+				return "YoutubeLive コメント投稿";
+			}
+		}
+
+		public override void Loaded()
+		{
+			mConfigManager = new ConfigManager();
+			mConfigManager.Load(Name);
+		}
+
+		public override void Closed()
+		{
+		}
+
+		public override void Setting()
+		{
+		}
+
+		public override void SpeechRecognitionStart()
+		{
+		}
+
+		public override void SpeechRecognitionStop()
+		{
+		}
+
+		public override void Speech(string text)
+		{
+		}
+	}
+}
