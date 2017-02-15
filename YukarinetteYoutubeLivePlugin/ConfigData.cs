4e771220fe18bf2be3e861c81ea1d8b821d85b82
midorigoke <midorigoke@desumachi.tk>
2017-02-15 20:39:58 +0900

    add: config

---
+++ YukarinetteYoutubeLivePlugin/ConfigData.cs
@@ -0,0 +1,30 @@
+﻿namespace YukarinetteYoutubeLivePlugin
+{
+	public class ConfigData
+	{
+		public string YoutubeChannelId
+		{
+			get;
+			set;
+		}
+
+		public string YoutubeApiKey
+		{
+			get;
+			set;
+		}
+
+		public int YoutubeTxDelay
+		{
+			get;
+			set;
+		}
+
+		public ConfigData()
+		{
+			YoutubeChannelId = "";
+			YoutubeApiKey = "";
+			YoutubeTxDelay = 0;
+		}
+	}
+}
